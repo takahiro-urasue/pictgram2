1e72325684fcbd8655483a58726edf0946a50af3
takahiro urasue <ura@takahiro-no-MacBook-Pro-5.local>
2019-03-27 00:57:58 +0900

    update

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,20 +1,12 @@
 class Topic < ApplicationRecord
   has_one_attached :image
   validates :user_id, presence: true
-  validates :description, presence: true
   validates :image, presence: true
-
+  validates :description, presence: true
   belongs_to :user
   has_many :favorite_users
-
   # mount_uploader :image, ImageUploader
-
-
-
   has_many :comments
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
-
-
-
 end
