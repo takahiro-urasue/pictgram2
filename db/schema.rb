fed4b084c276ac74cba01151b1ac957f98cde4dd
takahiro urasue <ura@takahiro-no-MacBook-Pro-5.local>
2019-04-01 19:21:51 +0900

    users profile photo db add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_064646) do
+ActiveRecord::Schema.define(version: 2019_04_01_014814) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_064646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "profile_photo"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
