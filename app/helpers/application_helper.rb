4cad31de15540a550c600eb126ed191126909692
takahiro urasue <ura@takahiro-no-MacBook-Pro-5.local>
2019-04-01 19:03:37 +0900

    avatar-url method add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def avatar_url user
+    return user.profile_photo unless user.profile_photo.blank?
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+  end
 
   def logged_in?
   !current_user.nil?
